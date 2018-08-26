b5117feedec07fedc37d756d23077afbf9bb62d3
mahmody <42693632+mahmody@users.noreply.github.com>
2018-08-26 11:02:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1251,7 +1251,7 @@ const secre = [
 
 
  client.on('message', message => {
-   if (message.content.startsWith("!لوخيروك")) {
+   if (message.content.startsWith("$لوخيروك")) {
                 if(!message.channel.guild) return message.reply('** This command only for servers**');
   var embed = new Discord.RichEmbed()
   .setColor('RANDOM')
