de14cc497c0f16867d436642420aa59792c97295
mahmody <42693632+mahmody@users.noreply.github.com>
2018-08-26 13:45:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1546,7 +1546,7 @@ client.on('message', async msg => {
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(prefix.length)
 
-	if (command === `$play`) {
+	if (command === `play`) {
 		const voiceChannel = msg.member.voiceChannel;
         
         if (!voiceChannel) return msg.channel.send("I can't find you in any voice channel!");
