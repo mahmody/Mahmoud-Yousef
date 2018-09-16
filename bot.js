41b67d8df330dd40cb834ed7e77892593c2fe53f
mahmody <42693632+mahmody@users.noreply.github.com>
2018-09-16 16:55:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1793,3 +1793,21 @@ client.on('message', message => {
       message.channel.send(helpEmbed);
     }
 });
+
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' الكلمة 1 ','الكلمة 2 | الكلمة 3','الكلمة 4 ','الكلمة 5','الكلمة 6'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`https://www.twitch.tv/n3k4a`);
+    }, ms);
+
+});
